551afdafb78b3d1f7b6a22adb5c2c19f3a3088ee
deniz2694 <deniz2694@gmail.com>
2017-09-28 21:04:10 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'DdYN5D1dhpI5M5Uu0UGvjGhtx',
+  consumer_secret:      'SiiXvePt8tGT4NjdTmUPLOgufoxDgwK2notX7EfeUz020pFvsw',
+  access_token:         '913463511396872193-pSBkiFVF8YiN4791Ab1itjzULHqawMX',
+  access_token_secret:  'TnVppITeLLeVgLDJruuNGCa4QazZm17EeH9Pz3JFWzP9J'
+}
+
+module.exports = config;
